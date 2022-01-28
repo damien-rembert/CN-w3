36040e69c5fd2049512a874bd7293b746e576b9c
Damien R <damien.rembert@gmail.com>
2022-01-28 17:27:19 +0000

    the animation slap CSS bit does not work

--- slap/app.js
+++ slap/app.js
@@ -12,6 +12,7 @@ const imageID = document.getElementById("face");
 
 // get sound
 const slap_sound = document.getElementById("slap_sound");
+const kiss_sound = document.getElementById("kiss_sound");
 
 // upload face rather than provide link?
 // const uploadID = document.getElementById("upload");
@@ -19,6 +20,9 @@ const slap_sound = document.getElementById("slap_sound");
 
 const inputID = document.getElementById("input");
 const buttonID = document.getElementById("submit");
+
+const handID = document.getElementById("hand");
+const handBoxID = document.getElementById("fg");
 
 // buttonID.addEventListener("click", () => {imageID.src = inputID.value;})
 buttonID.addEventListener("click", () => {face.src = inputID.value;})
@@ -38,7 +42,10 @@ buttonID.addEventListener("click", () => {face.src = inputID.value;})
 document.addEventListener("keydown", (event) => {
     if (checkSpace(event)) {
         slap();        
-    } 
+    }
+    if (checkK(event)) {
+        kiss();
+    }
     // else 
     // {
     //     alert(`you pressed: ${event.key}.\nThat's cool and all but only space works.`);
@@ -49,28 +56,57 @@ document.addEventListener("keydown", (event) => {
 // })
 
 function slap() {
-    // moveHand();
-    playSound();
+    moveHand();
+    playSound(slap_sound);
+}
+
+function kiss() {
+
+    playSound(kiss_sound);
 }
 
 function checkSpace(event) {
     // create a p to add to the div
     if (event.key == " ") {
         return true;        
-    } else return false;    
+    } else {
+        return false;
+    }   
+}
+
+function checkK(event) {
+    // create a p to add to the div
+    if ((event.key == "k") || (event.key == "K")) {
+        return true;        
+    } else {
+        return false;
+    }   
 }
 
 
 function moveHand() {
-    throw new Error("Function not implemented.");
+    handID.style.backgroundColor = "red";
+    handBoxID.style.animation = "slap 0.5s linear"
+    setTimeout(() => {
+        handBoxID.style.removeProperty('animation')
+        handID.style.backgroundColor = "";
+    }, 50);
 }
+// main.style.animation = "mainDrum 0.1s linear"
+//         setTimeout(() => {
+//             main.style.removeProperty('animation')
+//         }, 100)
 
-function playSound() {
+
+
+
+function playSound(sound) {
     // stop (in case already playing)
     // rewind
     // play
-    slap_sound.pause()
-    slap_sound.currentTime = 0
-    slap_sound.play();
+    sound.pause()
+    sound.currentTime = 0
+    sound.play();
 
 }
+
